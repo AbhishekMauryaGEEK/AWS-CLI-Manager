285f46a2388cbada257a9450fc5bdb8a095ffff6
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2026-06-23 11:51:16 +0000

    refactor: move core modules and entrypoints

---
+++ main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ./Core/config.sh
+source ./Core/install.sh
+source ./Core/auth.sh
+source ./Core/regions.sh
+
+source ./EC2/keypairs.sh
+source ./EC2/instances.sh
+source ./EC2/create_instance.sh
+
+source ./menu.sh
+
+check_aws_cli
+check_credentials
+select_region
+
+main_menu
